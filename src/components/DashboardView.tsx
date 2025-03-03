4c79568833e293f3f6e46f596c6280ae58ed313e
SwodLore <apovesmartinez@gmail.com>
2025-03-02 20:49:34 -0500

    Optimizar componentes

--- src/components/DashboardView.tsx
+++ src/components/DashboardView.tsx
@@ -21,8 +21,7 @@ export default function DashboardView() {
   return (
     <>
       <ScrollProgress className="lg:top-[80px] bg-[#61DAFB]" />
-      <WarpBackground className="bg-gray-900">
-      <section id="sobre-mi" className="text-white">
+      <section id="sobre-mi" className="bg-gray-900 text-white py-5">
         <div className="container mx-auto flex flex-col md:flex-row items-center gap-12">
           <div className="relative w-52 h-52 md:w-64 md:h-64">
           <BoxReveal boxColor={"#61DAFB"} duration={0.5}>
@@ -33,55 +32,36 @@ export default function DashboardView() {
             />
           </BoxReveal>
           </div>
-
+          <BoxReveal boxColor={"#ffffff"} duration={0.5}>
           <div className="flex-1 text-center md:text-left">
-            <BoxReveal boxColor={"#61DAFB"} duration={0.5}>
             <h2 className="text-4xl font-bold text-[#61DAFB] mb-4">Sobre Mí</h2>
-            </BoxReveal>
-            <BoxReveal boxColor={"#ffffff"} duration={0.5}>
               <p className="text-lg text-gray-300">
                 ¡Hola! Soy <span className="text-white font-semibold">Alessandro Poves</span>, un apasionado por el desarrollo web y la tecnología.  
                 Me especializo en backend con Laravel y Spring Boot, y estoy aprendiendo frontend con React y Angular.
               </p>
-            </BoxReveal>
 
             <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-4 text-center">
               <div className="bg-gray-800 p-4 rounded-lg border-l-4 border-[#61DAFB] shadow-md">
-                <BoxReveal boxColor={"#61DAFB"} duration={0.5}>
                 <img src="https://uncp.edu.pe/wp-content/uploads/2024/01/logo-uncp-2024.png" alt="Logo de la UNCP" />
-                </BoxReveal>
-                <BoxReveal boxColor={"#ffffff"} duration={0.5}>
                 <h3 className="text-xl font-semibold">  Universidad Nacional del Centro del Perú</h3>
-                </BoxReveal>
-                <BoxReveal boxColor={"#99a1af"} duration={0.5}>
                 <p className="text-gray-400">Ingeniería de Sistemas - 7mo ciclo</p>
-                </BoxReveal>
               </div>
               <div className="bg-gray-800 p-4 rounded-lg border-l-4 border-[#61DAFB] shadow-md">
-                <BoxReveal boxColor={"#61DAFB"} duration={0.5}>
                 <img src="https://aulavirtual.icpnarc.edu.pe/pluginfile.php/1/theme_moove/logo/1739894532/logo-icpna.jpg" alt="Logo ICPNA" />
-                </BoxReveal>
-                <BoxReveal boxColor={"#ffffff"} duration={0.5}>
                 <h3 className="text-xl font-semibold"> Instituto Cultural Peruano Norteamericano</h3>
-                </BoxReveal>
-                <BoxReveal boxColor={"#99a1af"} duration={0.5}>
                 <p className="text-gray-400">Ingles - Intermedio - TOEFL B2</p>
-                </BoxReveal>
               </div>
               <div className="bg-gray-800 p-4 rounded-lg border-l-4 border-[#61DAFB] shadow-md md:col-span-2">
-                <BoxReveal boxColor={"#ffffff"} duration={0.5}>
                 <h3 className="text-xl font-semibold">📜 Cursos Destacados</h3>
-                </BoxReveal>
-                <BoxReveal boxColor={"#99a1af"} duration={0.5}>
                 <p className="text-gray-400">Laravel, Spring Boot, Redes y Seguridad Informática {' '}
                   <button onClick={handleScrollToCertificado} className="text-[#61DAFB] hover:underline cursor-pointer">
                   Ver más
                   </button>  
                 </p>
-                </BoxReveal>
               </div>
             </div>
           </div>
+          </BoxReveal>
         </div>
         <div className="mt-12 flex flex-col items-center justify-center">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -119,23 +99,18 @@ export default function DashboardView() {
           </Link>
         </div>
     </section>
-    </WarpBackground>
     <WarpBackground className="bg-gray-800">
       <section id="portafolio" className="min-h-screen flex flex-col items-center text-white">
         <Portafolio />
       </section>
     </WarpBackground>
-      <WarpBackground className="bg-gray-700">
-        <section id="certificados" className="min-h-screen flex flex-col items-center  text-white">
+        <section id="certificados" className="bg-gray-700 min-h-screen flex flex-col items-center  text-white">
           <h2 className="text-4xl font-bold text-[#61DAFB] mt-5">Certificados</h2>
           <Certificado />
         </section>
-      </WarpBackground>
-      <WarpBackground className="bg-gray-600 p-3">
-        <section id="contacto" className="py-12 text-white">
+        <section id="contacto" className="py-12 bg-gray-600 text-white">
           <Contacto />
         </section>
-      </WarpBackground>
     </>
   )
 }
